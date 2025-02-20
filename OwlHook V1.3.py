24d75e4306e6a6032b73a5dcf9980217eff6f21d
elk <elkkerson@gmail.com>
2025-02-20 21:50:09 +0000

    Update OwlHook V1.3.py

--- OwlHook V1.3.py
+++ OwlHook V1.3.py
@@ -1,9 +1,12 @@
-# TOOL BY ELK
+# https://github.com/3elk/OwlHook
+# OWLHOOK BY ELK
 # GIVE ALL CREDIT TO ELK
+# OWLHOOK ON TOP
+
 import time
 import os
 import subprocess
-
+import platform
 
 try:
     import requests
@@ -20,18 +23,24 @@ except ModuleNotFoundError:
     from colorama import Fore, Back, Style
 
 try:
-    from dhooks import Webhook  # type: ignore
+    from dhooks import Webhook
 except (ModuleNotFoundError):
     print("Dhooks module not found, installing...")
     subprocess.check_call([os.sys.executable, "-m", "pip", "install", "dhooks"])
     from dhooks import Webhook
 
-os.system('mode 90,27')
-os.system('title {OwlHook - by elk}')
-import colorama
-from colorama import Fore,Back,Style
-colorama.init(autoreset = False)
+if platform.system() == "Windows":
+    os.system('mode 90,27')
+    os.system('title {OwlHook - by elk}')
+else:
+    os.system('printf "\e[8;27;90t"')
+    os.system('echo -ne "\033]0;{OwlHook - by elk}\007"') 
 
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=False)
+
+# ^^ imports
 
 banner = '''                                                  
 
@@ -53,37 +62,50 @@ banner = '''
                   ║                                                    ║
                   ║                                                    ║
                   ║                      99) - Exit                    ║
-                  ╚════════════════════════════════════════════════════╝ \n'''
+                  ╚════════════════════════════════════════════════════╝ \n\n'''
 print(f'{Fore.LIGHTYELLOW_EX}' + banner)
-os.system('echo       ╔═════HookOwl@%username%')
+if platform.system() == "Windows":
+    os.system('echo       ╔═════HookOwl@%username%')
+else:
+    os.system(f'echo       ╔═════HookOwl@{os.getlogin()}')
 print('      ║')
 choicehook = input('      ╚═════════════════════════>> ')
 
+# ^^ banner + input
 
 if choicehook not in ('1', '2', '99'):
-    print('Sorry, Not a valid option, Goodbye!')
-    os.system('timeout /t 3 /nobreak >nul')
-    os._exit
 
+    print('Sorry, Not a valid option, Goodbye!')
+    time.sleep(3)
+    os._exit(0)
+
+# ^^ if the input is not one of the listed options
 
 elif choicehook == '1':
-    os.system('cls; clear')
+
+# ^^ webhook-spammer option
+
+    os.system('cls' if platform.system() == 'Windows' else 'clear')
 
     print('''                                                                                                     
- _____ _ _   _____                             
-|   __| | |_|   __|___ ___ _____ 
-|   __| | '_|__   | . | .'|     |
-|_____|_|_,_|_____|  _|__,|_|_|_| 
-                  |_|                       
+     _____ _ _   _____                             
+    |   __| | |_|   __|___ ___ _____ 
+    |   __| | '_|__   | . | .'|     |
+    |_____|_|_,_|_____|  _|__,|_|_|_| 
+                      |_|                       
 
-        Webhook Spammer
+            Webhook-Spammer
           
                                                                       
 ''')
 
-    message0 = input("Spam message >> ")
-    webhookurl = Webhook(input("Webhook URL >> "))
+# ^^ webhook-spammer banner
+
+    message0 = input("    Message to spam >> ")
+    webhookurl = Webhook(input("\n    Webhook URL >> "))
     message = (message0 + ' - elk owns you :3 @here')
+
+# ^^ input for the webhook-spammer (the message, url, etc.)
 
     try:
         while True:
@@ -91,25 +113,30 @@ elif choicehook == '1':
             print("Sent MSG.")
     except KeyboardInterrupt:
         print("\nStopped spamming.")
-        os.system('pause >nul')
-        os._exit
+        input("Press Enter to continue . . .")
+        os._exit(0)
 
+# ^^ messaging the webhook using dhooks + if spamming is stopped
 
 elif choicehook == '2':
-    os.system('cls; clear')
-    print ('''                            
- _____ _ _    ____      _ 
-|   __| | |_ |    \ ___| |
-|   __| | '_||  |  | -_| |
-|_____|_|_,_||____/|___|_| 
 
-      Webhook Deleter
+# ^^ webhook-deleter option
+
+    os.system('cls' if platform.system() == 'Windows' else 'clear')
+    print ('''                            
+     _____ _ _    ____      _ 
+    |   __| | |_ |    \ ___| |
+    |   __| | '_||  |  | -_| |
+    |_____|_|_,_||____/|___|_| 
+
+          Webhook-Deleter
                              
 
 ''')
 
-    del_web = input("Enter the Webhook URL to delete >> ")
+    del_web = input("   Enter the Webhook URL to delete >> ")
 
+# ^^ webhook-deleter banner + webhook URL input
 
     def delete(del_web):
         requests.delete(del_web)
@@ -121,7 +148,6 @@ elif choicehook == '2':
             print("\nCould not delete the Webhook.")
             time.sleep(3.5)
 
-
     test = requests.get(del_web)
     if test.status_code == 404:
         print("\nInvalid webhook.")
@@ -130,8 +156,14 @@ elif choicehook == '2':
         print("\nValid Hook...")
         delete(del_web)
 
+# ^^ webhook-deleters code (Checking if webhook is valid by status code + using the requests to delete the webhook)
 
 elif choicehook == '99':
-    print ('                                                Exiting OwlHook, Goodbye!')
-    os.system('timeout /t 001 /nobreak >nul')
-    os._exit
+
+# ^^ exit option
+
+    print ('                                Exiting OwlHook, Goodbye!')
+    time.sleep(1)
+    os._exit(0)
+
+# ^^ ts pmo icl 808 r u fr rn nigbart 
