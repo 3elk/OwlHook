01be44c5f1429516ea6f8c664ad34754fc10da6a
elk <elkkerson@gmail.com>
2025-02-23 05:03:43 +0000

    Update OwlHook V1.4.py

--- OwlHook V1.4.py
+++ OwlHook V1.4.py
@@ -1,46 +1,80 @@
-# https://github.com/3elk/OwlHook
-# OWLHOOK BY ELK
-# GIVE ALL CREDIT TO ELK
-# OWLHOOK ON TOP
-
 import time
 import os
 import subprocess
 import platform
+import itertools
+import functools
+import threading
+import random
+import string
+import json
+import asyncio
 
-try:
-    import requests
-except ModuleNotFoundError:
-    print("Requests module not found, installing...")
-    subprocess.check_call([os.sys.executable, "-m", "pip", "install", "requests"])
-    import requests
+def install_module(module_name):
+    try:
+        subprocess.check_call([os.sys.executable, "-m", "pip", "install", module_name])
+    except subprocess.CalledProcessError:
+        print(f"Failed to install module {module_name}")
 
-try:
-    from colorama import Fore, Back, Style
-except ModuleNotFoundError:
-    print("Colorama module not found, installing...")
-    subprocess.check_call([os.sys.executable, "-m", "pip", "install", "colorama"])
-    from colorama import Fore, Back, Style
+def import_requests():
+    try:
+        import requests
+        return requests
+    except ModuleNotFoundError:
+        print("Requests module not found, installing...")
+        install_module("requests")
+        import requests
+        return requests
 
-try:
-    from dhooks import Webhook
-except (ModuleNotFoundError):
-    print("Dhooks module not found, installing...")
-    subprocess.check_call([os.sys.executable, "-m", "pip", "install", "dhooks"])
-    from dhooks import Webhook
+requests = import_requests()
 
-if platform.system() == "Windows":
-    os.system('mode 90,27')
-    os.system('title {OwlHook - by elk}')
-else:
-    os.system('printf "\e[8;27;90t"')
-    os.system('echo -ne "\033]0;{OwlHook - by elk}\007"') 
+def import_colorama():
+    try:
+        from colorama import Fore, Back, Style
+        return Fore, Back, Style
+    except ModuleNotFoundError:
+        print("Colorama module not found, installing...")
+        install_module("colorama")
+        from colorama import Fore, Back, Style
+        return Fore, Back, Style
+
+Fore, Back, Style = import_colorama()
+
+def import_dhooks():
+    try:
+        from dhooks import Webhook
+        return Webhook
+    except ModuleNotFoundError:
+        print("Dhooks module not found, installing...")
+        install_module("dhooks")
+        from dhooks import Webhook
+        return Webhook
+
+Webhook = import_dhooks()
+
+def set_terminal_title(title):
+    if platform.system() == "Windows":
+        os.system(f'title {title}')
+    else:
+        os.system(f'echo -ne "\033]0;{title}\007"')
+
+def set_terminal_size_and_title():
+    if platform.system() == "Windows":
+        os.system('mode 90,27')
+    else:
+        os.system('printf "\e[8;27;90t"')
+    set_terminal_title('OwlHook - by elk')
+
+set_terminal_size_and_title()
 
 import colorama
-from colorama import Fore, Back, Style
 colorama.init(autoreset=False)
 
-# ^^ imports
+def clear_terminal():
+    os.system('cls' if platform.system() == 'Windows' else 'clear')
+
+clear_terminal()
+
 banner = '''                                                  
 
                                  
@@ -55,36 +89,53 @@ banner = '''
                   ======================================================
                   ╔════════════════════════════════════════════════════╗
                   ║                                                    ║
-                  ║        1) - Spammer              3) - W.H Info     ║
+                  ║        1) - Spammer            3) - WebHook Info   ║
                   ║                                                    ║
                   ║        2) - Deleter                                ║
                   ║                                                    ║
                   ║                                                    ║
                   ║                      99) - Exit                    ║
                   ╚════════════════════════════════════════════════════╝ \n\n'''
-print(f'{Fore.LIGHTYELLOW_EX}' + banner)
-if platform.system() == "Windows":
-    os.system('echo       ╔═════HookOwl@%username%')
-else:
-    os.system(f'echo       ╔═════HookOwl@{os.getlogin()}')
-print('      ║')
-choicehook = input('      ╚═════════════════════════>> ')
 
-# ^^ banner + input
+def print_banner():
+    print(f'{Fore.LIGHTYELLOW_EX}' + banner)
 
-if choicehook not in ('1', '2', '3', '99'):
+def print_username():
+    if platform.system() == "Windows":
+        os.system('echo       ╔═════HookOwl@%username%')
+    else:
+        os.system(f'echo       ╔═════HookOwl@{os.getlogin()}')
+    print('      ║')
 
-    print('Sorry, Not a valid option, Goodbye!')
+def inval():
+    print('Sorry. Not a valid option, try again!')
     time.sleep(2)
+    main()
 
-# ^^ if the input is not one of the listed options
+def exiting():
+    print('                                Exiting OwlHook, Goodbye!')
+    time.sleep(1)
+    os._exit(0)
 
-elif choicehook == '1':
-
-# ^^ webhook-spammer option
-
-    os.system('cls' if platform.system() == 'Windows' else 'clear')
-
+def main():
+    clear_terminal()
+    print_banner()
+    print_username()
+    choicehook = input('      ╚═════════════════════════>> ')
+    
+    if choicehook not in ('1', '2', '3', '99'):
+        inval()
+    elif choicehook == '99':
+        exiting()
+    elif choicehook == '1':
+        spam()
+    elif choicehook == '2':
+        deleter()
+    elif choicehook == '3':
+        webhook_info()
+    
+def spam():
+    clear_terminal()
     print('''                                                                                                     
      _____ _ _   _____                             
     |   __| | |_|   __|___ ___ _____ 
@@ -97,13 +148,9 @@ elif choicehook == '1':
                                                                       
 ''')
 
-# ^^ webhook-spammer banner
-
     message0 = input("    Message to spam >> ")
     webhookurl = Webhook(input("\n    Webhook URL >> "))
     message = (message0 + ' - elk owns you :3 @here')
-
-# ^^ input for the webhook-spammer (the message, url, etc.)
 
     try:
         while True:
@@ -113,13 +160,8 @@ elif choicehook == '1':
         print("\nStopped spamming.")
         input("Press Enter to continue . . .")
 
-# ^^ messaging the webhook using dhooks + if spamming is stopped
-
-elif choicehook == '2':
-
-# ^^ webhook-deleter option
-
-    os.system('cls' if platform.system() == 'Windows' else 'clear')
+def deleter():
+    clear_terminal()
     print ('''                            
      _____ _ _    ____      _ 
     |   __| | |_ |    \ ___| |
@@ -130,16 +172,13 @@ elif choicehook == '2':
                              
 
 ''')
-
     del_web = input("   Enter the Webhook URL to delete >> ")
-
-# ^^ webhook-deleter banner + webhook URL input
 
     def delete(del_web):
         requests.delete(del_web)
         check = requests.get(del_web)
         if check.status_code == 404:
-            print("\nWebhook Successfully Deleted")
+            print("\nWebhook Successfully Deleted!")
             time.sleep(2)
             main()
         elif check.status_code == 200:
@@ -155,11 +194,9 @@ elif choicehook == '2':
         print("\nValid Hook...")
         delete(del_web)
 
-# ^^ webhook-deleters code (Checking if webhook is valid by status code + using the requests to delete the webhook)
-
-elif choicehook == '3':
-	os.system('cls' if platform.system() == 'Windows' else 'clear')
-	webhookinfo = input('''                     
+def webhook_info():
+    clear_terminal()
+    webhookinfo = input('''                     
      _____ _ _   _____     ___     
     |   __| | |_|     |___|  _|___ 
     |   __| | '_|-   -|   |  _| . |
@@ -170,28 +207,30 @@ elif choicehook == '3':
 
 
     Webhook URL to Find Info on >> ''')
-	print('\nID >> ' + requests.get(webhookinfo).json()['id'])
 
-	print('User >> ' + requests.get(webhookinfo).json()['name'])
+    response = requests.get(webhookinfo).json()
+    print('\n    ID >> ' + response['id'])
+    print('    User >> ' + response['name'])
+    print('    Avatar >> ' + str(response['avatar']))
+    print('    Channel ID >> ' + response['channel_id'])
+    print('    Guild ID >> ' + response['guild_id'])
+    print('    App ID >> ' + str(response['application_id']))
+    print('    Token >> ' + response['token'])
+    input('\n    Press Enter To Continue . . .')
 
-	print('Avatar >> ' + str(requests.get(webhookinfo).json()['avatar']))
+def start():
+    def nested_start():
+        banner_thread = threading.Thread(target=print_banner)
+        banner_thread.start()
+        banner_thread.join()
+        main_thread = threading.Thread(target=main)
+        main_thread.start()
+        main_thread.join()
 
-	print('Channel ID >> ' + requests.get(webhookinfo).json()['channel_id'])
+    nested_start()
 
-	print('Guild ID >> ' + requests.get(webhookinfo).json()['guild_id'])
+async def async_main():
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(None, start)
 
-	print('App ID >> ' + str(requests.get(webhookinfo).json()['application_id']))
-
-	print('Token >> ' + requests.get(webhookinfo).json()['token'])
-    
-	input('Press Enter To Continue . . .')
-
-elif choicehook == '99':
-
-# ^^ exit option
-
-    print ('                                Exiting OwlHook, Goodbye!')
-    time.sleep(1)
-    os._exit(0)
-
-# ^^ ts pmo icl 808 r u fr rn nigbart
+asyncio.run(async_main())
